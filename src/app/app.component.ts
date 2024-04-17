447bfb67cb24e0d9a12596fc1c28a8b12f89543d
MigVarona <migvaronag@gmail.com>
2024-04-18 00:19:57 +0200

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import emailjs from '@emailjs/browser';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactUsComponent, HeaderComponent, AboutComponent ],
+  imports: [RouterOutlet, ContactUsComponent, HeaderComponent, AboutComponent, ProjectsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
