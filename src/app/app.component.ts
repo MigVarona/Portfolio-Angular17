a43ba6746c80019516c065d943949f2e75ab9a67
MigVarona <migvaronag@gmail.com>
2024-04-18 01:11:10 +0200

    pr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import emailjs from '@emailjs/browser';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
